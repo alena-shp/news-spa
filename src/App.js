a4a4c42ae7ce50af274e5b74936fc561a7e04f73
Shpakova Alena <shpakovadev@gmail.com>
2020-03-16 21:41:40 +0300

    add header

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
-import React from 'react';
-import './App.css';
+import React from "react"
+import "./App.css"
+import Header from "./components/header"
 
 function App() {
   return (
     <div className="App">
-     
+      <Header />
+      Greeting/ News
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
---
+++ src/components/header/header.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+const Header = () => {
+  return (
+    <div className="header">
+      <ul className="header__menu">
+        <li>
+          <a to="/">Главная</a>
+        </li>
+        <li>
+          <a to="/news/">Новости</a>
+        </li>
+        <li>
+          <a to="/enter/">Вход/Выход</a>
+        </li>
+      </ul>
+    </div>
+  )
+}
+
+export default Header
