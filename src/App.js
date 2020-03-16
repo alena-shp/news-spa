2ce616dcd979196218d9873b64239f94f68be5cc
Shpakova Alena <shpakovadev@gmail.com>
2020-03-16 23:45:44 +0300

    routing

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
 import React from 'react'
 import './App.css'
+import { BrowserRouter as Router } from 'react-router-dom'
+import { Route } from 'react-router-dom'
+
 import Header from './components/header'
 import News from './components/news'
 import Greeting from './components/greeting'
@@ -7,10 +10,11 @@ import Greeting from './components/greeting'
 function App() {
   return (
     <div className="app">
-      <Header />
-      <Greeting />
-      <News />
-      Greeting/ News
+      <Router>
+        <Header />
+        <Route path="/" component={Greeting} exact />
+        <Route path="/news" component={News} />
+      </Router>
     </div>
   )
 }
--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,18 +1,20 @@
 import React from 'react'
 import './header.scss'
 
+import { Link } from 'react-router-dom'
+
 const Header = () => {
   return (
     <div className="header">
       <ul className="header__menu">
         <li>
-          <a to="/">Главная</a>
+          <Link to="/" className="header__link">Главная</Link>
         </li>
         <li>
-          <a to="/news/">Новости</a>
+          <Link to="/news" className="header__link">Новости</Link>
         </li>
         <li>
-          <a to="/enter/">Вход/Выход</a>
+          <a href="0#" className="header__link">Вход/Выход</a>
         </li>
       </ul>
     </div>
