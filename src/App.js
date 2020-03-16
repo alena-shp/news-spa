86dd4e5de49ad0b5529032986fd0cd92fa1984d8
Shpakova Alena <shpakovadev@gmail.com>
2020-03-16 23:33:38 +0300

    add greeting component

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React from 'react'
 import './App.css'
 import Header from './components/header'
 import News from './components/news'
+import Greeting from './components/greeting'
 
 function App() {
   return (
     <div className="app">
       <Header />
+      <Greeting />
       <News />
       Greeting/ News
     </div>
---
+++ src/components/greeting/greeting.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './greeting.scss'
+
+const Greeting = () => {
+  return (
+    <div className="greeting">
+      <p>Привет, гость!</p>
+    </div>
+  )
+}
+
+export default Greeting
